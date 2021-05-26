629c829e9d6ac9672870585c9e238ba856a30e97
lucmicaelli <luc.micaelli@gmail.com>
2021-05-25 21:04:06 -0400

    added logic to decide winner

--- game.py
+++ game.py
@@ -15,4 +15,34 @@ valid_options = ["Rock","Paper","Scissors"]
 computer_choice = random.choice(valid_options)
 print ("the computer choice is :", computer_choice)
 
+if (user_choice == "Rock") and (computer_choice == "Scissors"):
+    print ("You WIN!")
+
+elif (user_choice == "Paper") and (computer_choice == "Rock"):
+    print ("You WIN!")
+
+elif (user_choice == "Scissors") and (computer_choice == "Paper"):
+    print ("You WIN!")
+
+elif (computer_choice == "Rock") and (user_choice == "Scissors"):
+    print ("You LOSE!")
+
+elif (computer_choice == "Paper") and (user_choice == "Rock"):
+    print ("You LOSE!")
+
+elif (computer_choice == "Scissors") and (user_choice == "Paper"):
+    print ("You LOSE!")
+
+elif (computer_choice == "Rock") and (user_choice == "Rock"):
+    print ("TIE!")
+
+elif (computer_choice == "Paper") and (user_choice == "Paper"):
+    print ("TIE!")
+
+elif (computer_choice == "Scissors") and (user_choice == "Scissors"):
+    print ("TIE!")
+
+else:
+    print ("ERROR")
+
 print ("THIS IS THE END OF OUR GAME, PLEASE PLAY AGAIN")
