dd026dc5cd7cce36579240c9aa63c59e0dcf071c
lucmicaelli <luc.micaelli@gmail.com>
2021-05-25 20:52:29 -0400

    added random computer choice

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # game.py
+import random
 
 print("Rock, Paper, Scissors, Shoot!")
 user_choice = input ("Choose one of 'Rock', 'Paper', 'Scissors': ")
@@ -9,5 +10,9 @@ if (user_choice == "Rock") or (user_choice == "Paper") or (user_choice == "Sciss
 else:
     print("This is not a valid entry... try again!")
     quit()
+
+valid_options = ["Rock","Paper","Scissors"]
+computer_choice = random.choice(valid_options)
+print ("the computer choice is :", computer_choice)
 
 print ("THIS IS THE END OF OUR GAME, PLEASE PLAY AGAIN")
