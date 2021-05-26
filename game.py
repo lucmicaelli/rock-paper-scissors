18bd57f2d2a066932e2e6d53d6b2d58dac487cc2
lucmicaelli <luc.micaelli@gmail.com>
2021-05-25 20:38:17 -0400

    user input and validating entry

--- game.py
+++ game.py
@@ -1,5 +1,13 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
-user_choice = input ("Choose one of 'rock', 'paper', 'scissors': ")
-print("Your Choice: ", user_choice)
+user_choice = input ("Choose one of 'Rock', 'Paper', 'Scissors': ")
+
+if (user_choice == "Rock") or (user_choice == "Paper") or (user_choice == "Scissors"):
+    print("VALID entry, Your Choice: ", user_choice)
+
+else:
+    print("This is not a valid entry... try again!")
+    quit()
+
+print ("THIS IS THE END OF OUR GAME, PLEASE PLAY AGAIN")
